bb296065e4ebc6fca614cd1dcf1fd5935f25dcf4
Rositsa Nenova <93535482+NenovaRositsa@users.noreply.github.com>
2021-11-05 12:51:31 +0200

    Add files via upload

---
+++ 01IntroAndBasicSyntax/07Vending Machine/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = 0.00;
+            string input = Console.ReadLine();
+
+            while (input != "Start")
+            {
+                double coin = double.Parse(input);
+
+                if (coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1 || coin == 2)
+                {
+                    money += coin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string product = Console.ReadLine();
+
+            while (product != "End")
+            {
+                double price = 0.00;
+
+                if (product != "Nuts" && product != "Water" && product != "Crisps" && product != "Soda" && product != "Coke")
+                {
+                    Console.WriteLine("Invalid product");
+                    product = Console.ReadLine();
+                    continue;
+                }
+
+                switch (product)
+                {
+                    case "Nuts":
+                        price = 2.0;
+                        break;
+                    case "Water":
+                        price = 0.7;
+                        break;
+                    case "Crisps":
+                        price = 1.5;
+                        break;
+                    case "Soda":
+                        price = 0.8;
+                        break;
+                    case "Coke":
+                        price = 1.0;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (price <= money)
+                {
+                    Console.WriteLine($"Purchased {product.ToLower()}");
+                    money -= price;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                }
+
+                product = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Change: {money:f2}");
+        }
+    }
+}
