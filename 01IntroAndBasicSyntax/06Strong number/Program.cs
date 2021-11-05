b209e28f7dc5211f09ce9dbf0e1650b83816bc96
Rositsa Nenova <93535482+NenovaRositsa@users.noreply.github.com>
2021-11-05 12:51:11 +0200

    Add files via upload

---
+++ 01IntroAndBasicSyntax/06Strong number/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currentDigit = int.Parse(input[i].ToString());
+                int currentFactorial = 1;
+
+                for (int j = 1; j <= currentDigit; j++)
+                {
+                    currentFactorial *= j;
+                }
+
+                sum += currentFactorial;
+            }
+
+            if (sum == number)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
