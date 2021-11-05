10cda2ef347960b3cd9b4bfb31109b07f749d623
Rositsa Nenova <93535482+NenovaRositsa@users.noreply.github.com>
2021-11-05 12:51:52 +0200

    Add files via upload

---
+++ 01IntroAndBasicSyntax/08Triangle of Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int r = 1; r <= i; r++)
+                {
+                    Console.Write($"{i} ");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
