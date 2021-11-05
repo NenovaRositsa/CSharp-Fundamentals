988eb93dbfc37b20d8bf98fbac9b0ff6f3b1712d
Rositsa Nenova <93535482+NenovaRositsa@users.noreply.github.com>
2021-11-05 12:50:28 +0200

    Add files via upload

---
+++ 01IntroAndBasicSyntax/04Print and sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
